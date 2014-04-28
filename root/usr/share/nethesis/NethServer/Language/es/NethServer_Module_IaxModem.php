84f5005710702b586bf584549970fc4b691bf8a9
Victor Moreno <victor_moreno_rubio@hotmail.com>
2014-04-28 16:43:35 +0200

    Web interface: add Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_IaxModem.php
@@ -0,0 +1,14 @@
+<?php 
+
+/* NethServer_Module_IaxModem translation, language: en */
+
+
+$L['Key_label'] = 'Nombre'; 
+$L['IaxModem_Description'] = 'Configurar IAX modems'; 
+$L['IaxModem_Tags'] = 'iax iax2 modem modems'; 
+$L['IaxModem_Title'] = 'Modems IAX '; 
+$L['cidName_label'] = 'Número de llamadas'; 
+$L['cidNumber_label'] = 'Número de llamadas'; 
+$L['extension_label'] = 'Extensión'; 
+$L['name_label'] = 'Nombre'; 
+$L['server_label'] = 'Servidor IP'; 
